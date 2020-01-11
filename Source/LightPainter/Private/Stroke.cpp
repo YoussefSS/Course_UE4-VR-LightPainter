10987d67891df75356e111d6decaec405c15199d
YoussefSS <joesamir98@gmail.com>
2020-01-11 22:46:43 +0200

    37. Reducing DrawCalls With Instancing

--- Source/LightPainter/Private/Stroke.cpp
+++ Source/LightPainter/Private/Stroke.cpp
@@ -10,27 +10,17 @@ AStroke::AStroke()
 
 	Root = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
 	SetRootComponent(Root);
+
+	StrokeMeshes = CreateDefaultSubobject<UInstancedStaticMeshComponent>(TEXT("StrokeMeshes"));
+	StrokeMeshes->SetupAttachment(Root);
 }
 
 void AStroke::Update(FVector CursorLocation)
 {
-	USplineMeshComponent* Spline = CreateSplineMesh();
-	
-	FVector StartPosition = GetActorTransform().InverseTransformPosition(CursorLocation); // InverseTransform transforms from world space to local space ..
-	FVector EndPosition = GetActorTransform().InverseTransformPosition(PreviousCursorLocation); // .. can use Transform.TransformPoint to convert from local to world space
-	Spline->SetStartAndEnd(StartPosition, FVector::ZeroVector, EndPosition, FVector::ZeroVector);
+	FTransform NewStrokeTransform;
+	FVector LocalCursorLocation = GetTransform().InverseTransformPosition(CursorLocation);
+	NewStrokeTransform.SetLocation(LocalCursorLocation);
+	StrokeMeshes->AddInstance(NewStrokeTransform);
 
 	PreviousCursorLocation = CursorLocation;
-}
-
-USplineMeshComponent* AStroke::CreateSplineMesh()
-{
-	USplineMeshComponent* NewSpline = NewObject<USplineMeshComponent>(this);
-	NewSpline->SetMobility(EComponentMobility::Movable);
-	NewSpline->AttachToComponent(Root, FAttachmentTransformRules::SnapToTargetIncludingScale);
-	NewSpline->SetStaticMesh(SplineMesh);
-	NewSpline->SetMaterial(0, SplineMaterial);
-	NewSpline->RegisterComponent();
-
-	return NewSpline;
 }
