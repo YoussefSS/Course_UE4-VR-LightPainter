4c6a62d5bd5805d11addf888a3845f39f877be15
YoussefSS <joesamir98@gmail.com>
2020-01-19 18:08:08 +0200

    63. Loading Save Games In GameMode

--- Source/LightPainter/Private/PaintingGameMode.cpp
+++ Source/LightPainter/Private/PaintingGameMode.cpp
@@ -3,6 +3,8 @@
 
 #include "Public/PaintingGameMode.h"
 #include "Kismet/GameplayStatics.h"
+#include "Kismet/StereoLayerFunctionLibrary.h"
+#include "Public/Saving/PainterSaveGame.h"
 
 void APaintingGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
 {
@@ -11,4 +13,21 @@ void APaintingGameMode::InitGame(const FString& MapName, const FString& Options,
 	SlotName = UGameplayStatics::ParseOption(Options, "SlotName");
 
 	UE_LOG(LogTemp, Warning, TEXT("SlotName: %s"), *SlotName);
+}
+
+void APaintingGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+	UPainterSaveGame* Painting = UPainterSaveGame::Load(SlotName);
+	if (Painting)
+	{
+		Painting->DeserializeToWorld(GetWorld());
+		UStereoLayerFunctionLibrary::HideSplashScreen();// just following along, in case we had a splash screen
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Game slot not found: %s"), *SlotName);
+	}
 }
--- Source/LightPainter/VRPawn.h
+++ Source/LightPainter/VRPawn.h
@@ -26,7 +26,6 @@ private:
 	void RightTriggerReleased() { if (RightPaintBrushHandController) RightPaintBrushHandController->TriggerReleased(); }
 
 	void Save();
-	void Load();
 
 	// Config
 	UPROPERTY(EditDefaultsOnly)
